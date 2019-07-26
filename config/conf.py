1469125d9ff05c42660b69a22d17b8431fec7a62
Alexandr <ieaiaio@bk.ru>
2019-07-26 11:31:50 +0300

    change config

--- config/conf.py
+++ config/conf.py
@@ -1,9 +1,11 @@
 class Config:
-    # DATABASE = 'postgres://bcpunbmalhoelx:0b5c8be713e5c154ad5df8962748329af694897313b3a5edaa05565ee4fc8c51@ec2-54-247-72-30.eu-west-1.compute.amazonaws.com:5432/deeip25epu1jg8'
-    DATABASE = 'postgresql://postgres:1234@127.0.0.1/dstubot'
+    DATABASE = 'postgres://bcpunbmalhoelx:0b5c8be713e5c154ad5df8962748329af694897313b3a5edaa05565ee4fc8c51@ec2-54-247-72-30.eu-west-1.compute.amazonaws.com:5432/deeip25epu1jg8'
+    # DATABASE = 'postgresql://postgres:1234@127.0.0.1/dstubot'
     VK_TOKEN ='1fe7ce1542f80ded456d1c65dc8daca77aa29e539977159f1a9501bf2bd789f960c4497fea25713e6bd57'
 
     SECRET_KEY = 'qwerrt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@127.0.0.1/dstubot'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@127.0.0.1/dstubot'
+    SQLALCHEMY_DATABASE_URI = 'postgres://bcpunbmalhoelx:0b5c8be713e5c154ad5df8962748329af694897313b3a5edaa05565ee4fc8c51@ec2-54-247-72-30.eu-west-1.compute.amazonaws.com:5432/deeip25epu1jg8'
+
     # FLASK_ADMIN_SWATCH = 'cerulean'
