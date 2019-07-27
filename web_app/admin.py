4e480b742830c708b1a8407fe99a9ce648863f36
Alexandr <ieaiaio@bk.ru>
2019-07-27 13:16:48 +0300

    add imports for admin panel

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,30 +1,33 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
-from app.models.user_models import User
+from app.models.faculty_models import Faculty, Dean, ScheduleDeanOffice, Department, ManagerDepartment, Specialty, \
+    TypeSpecialty
+from app.models.place_models import Place, TypePlace, SchedulePlace, ManagerPlace, Post
+from app.models.user_models import User, UserCache, UserAnswer, Review
 from web_app import flask_app, db
 
 admin = Admin(flask_app, name='DataBase', url='/')
 
 admin.add_view(ModelView(User, db.session, name='Пользователи', category='Пользователи'))
-# admin.add_view(ModelView(UserCache, db.session, name='Кэш пользователей', category='Пользователи'))
-# admin.add_view(ModelView(UserAnswer, db.session, name='Ответы пользователей', category='Пользователи'))
-# admin.add_view(ModelView(Review, db.session, name='Отзывы', category='Пользователи'))
-#
-# admin.add_view(ModelView(Place, db.session, name='Места', category='Места'))
-# admin.add_view(ModelView(TypePlace, db.session, name='Тип места', category='Места'))
-# admin.add_view(ModelView(SchedulePlace, db.session, name='Расписание', category='Места'))
-# admin.add_view(ModelView(ManagerPlace, db.session, name='Управляющий', category='Места'))
-# admin.add_view(ModelView(Post, db.session, name='Должность управляющего', category='Места'))
-#
-# admin.add_view(ModelView(Faculty, db.session, name='Факультеты', category='Структура университета'))
-# admin.add_view(ModelView(Dean, db.session, name='Деканы', category='Структура университета'))
-# admin.add_view(
-#     ModelView(ScheduleDeanOffice, db.session, name='Расписание деканата', category='Структура университета'))
-# admin.add_view(ModelView(Department, db.session, name='Кафедра', category='Структура университета'))
-# admin.add_view(
-#     ModelView(ManagerDepartment, db.session, name='Управляющие кафедрами', category='Структура университета'))
-# admin.add_view(ModelView(Specialty, db.session, name='Специальности', category='Структура университета'))
-# admin.add_view(ModelView(TypeSpecialty, db.session, name='Типы специальностей', category='Структура университета'))
+admin.add_view(ModelView(UserCache, db.session, name='Кэш пользователей', category='Пользователи'))
+admin.add_view(ModelView(UserAnswer, db.session, name='Ответы пользователей', category='Пользователи'))
+admin.add_view(ModelView(Review, db.session, name='Отзывы', category='Пользователи'))
+
+admin.add_view(ModelView(Place, db.session, name='Места', category='Места'))
+admin.add_view(ModelView(TypePlace, db.session, name='Тип места', category='Места'))
+admin.add_view(ModelView(SchedulePlace, db.session, name='Расписание', category='Места'))
+admin.add_view(ModelView(ManagerPlace, db.session, name='Управляющий', category='Места'))
+admin.add_view(ModelView(Post, db.session, name='Должность управляющего', category='Места'))
+
+admin.add_view(ModelView(Faculty, db.session, name='Факультеты', category='Структура университета'))
+admin.add_view(ModelView(Dean, db.session, name='Деканы', category='Структура университета'))
+admin.add_view(
+    ModelView(ScheduleDeanOffice, db.session, name='Расписание деканата', category='Структура университета'))
+admin.add_view(ModelView(Department, db.session, name='Кафедра', category='Структура университета'))
+admin.add_view(
+    ModelView(ManagerDepartment, db.session, name='Управляющие кафедрами', category='Структура университета'))
+admin.add_view(ModelView(Specialty, db.session, name='Специальности', category='Структура университета'))
+admin.add_view(ModelView(TypeSpecialty, db.session, name='Типы специальностей', category='Структура университета'))
 
 
