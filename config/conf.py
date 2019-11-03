1a7a3d7175cc22c4c777f7b56b5cd2cb12c5464c
Alexandr <ieaiaio@bk.ru>
2019-11-04 00:46:11 +0800

    for testing

--- config/conf.py
+++ config/conf.py
@@ -3,7 +3,13 @@ class Config:
 
     SECRET_KEY = 'qwerrt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = DATABASE = 'postgresql://postgres:1234@127.0.0.1/dstubot_test'
     SQLALCHEMY_DATABASE_URI = DATABASE = 'postgresql://postgres:1234@127.0.0.1/dstubot'
     # SQLALCHEMY_DATABASE_URI = DATABASE = 'postgres://cmalhdadykrvvc:645ec287e2d2c1593dfcf429a4738e5a022db27999a9d3f02ca260bc714f11d6@ec2-54-217-219-235.eu-west-1.compute.amazonaws.com:5432/dfh21ke69pne2k'
 
     # FLASK_ADMIN_SWATCH = 'cerulean'
+
+    # set FLASK_APP=web_app.run:flask_app
+    # flask db init
+    # flask db migrate
+    # flask db upgrade
