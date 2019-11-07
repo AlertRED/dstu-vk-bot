b1147f76a6099da86926327c7a35e30ae999aefe
Alexandr <ieaiaio@bk.ru>
2019-11-07 20:38:59 +0800

    add support methods

--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,24 @@
 import web_app.flask_app as flask_app
+from datetime import datetime, date, time
 
 db = flask_app.db
 days_of_week = ('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс')
 days_of_week_enum = db.Enum(*days_of_week, name="days_of_week")
+pairs_time = (time(8, 30), time(10, 15), time(12, 00), time(14, 15), time(16, 00), time(17, 45), time(19, 30))
+
+
+def now_semester():
+    month = datetime.now().month
+    return 2 if (month >= 2 and not month >= 9) else 1
+
+
+def now_week():
+    a = date(datetime.now().year, 9, 1)
+    offset = a.weekday() if a.weekday() < 6 else -1
+    b = datetime.now().date()
+    days = (b - a).days + offset
+    return 1 if ((days // 7) % 2 == 0) else 2
+
 
 from app.models.models_user import *
 from app.models.models_faculty import *
