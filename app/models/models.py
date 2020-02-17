a238451e6d6e76465046a62f517df261251ea07f
Alexandr <ieaiaio@bk.ru>
2020-02-17 21:35:35 +0800

    small refactor

--- app/models/models.py
+++ app/models/models.py
@@ -7,12 +7,12 @@ days_of_week_enum = db.Enum(*days_of_week, name="days_of_week")
 pairs_time = (time(8, 30), time(10, 15), time(12, 00), time(14, 15), time(16, 00), time(17, 45), time(19, 30))
 
 
-def now_semester():
+def current_semester():
     month = datetime.now().month
     return 2 if (month >= 2 and not month >= 9) else 1
 
 
-def now_week():
+def current_week():
     a = date(datetime.now().year, 9, 1)
     offset = a.weekday() if a.weekday() < 6 else -1
     b = datetime.now().date()
--- distribution/distribution_app.py
+++ distribution/distribution_app.py
@@ -1,18 +1,47 @@
 from time import sleep
 
+from vk_api import VkApi
+
 from app.models.models import *
 from datetime import datetime
 
-while True:
-    (min_time,), users = User.get_users_remind()
-    if users:
-        delay = (min_time - datetime.now()).seconds
-        print('Жду %s секунд' % delay)
-        sleep(2)
-        weekday = min_time.weekday()
-        for i, user in enumerate(users):
-            print(user.get_group().get_schedule(day=days_of_week[weekday], week=now_week(), semester=now_semester()))
-            user.refresh_nearest_remind()
-    else:
-        print('Ничего не нашел, сплю 10 секунд')
-        sleep(10)
+
+class DistributionApp:
+    __timeout = 60
+
+    def __init__(self, vk: VkApi):
+        self.vk = vk
+
+    # Возвращает ближайших по времени пользователей для отправки им расписания
+    @staticmethod
+    def get_near_time_users():
+        while True:
+            (min_time,), users = User.get_users_remind()
+            if not min_time:
+                sleep(DistributionApp.__timeout)
+                continue
+            if min_time > datetime.now():
+                break
+            else:
+                for user in users:
+                    user.refresh_nearest_remind()
+        return min_time, users
+
+    # Осправляет сообщения ближайшим по времени пользователям с их расписанием и обновляет их время
+    @staticmethod
+    def run():
+        while True:
+            min_time, users = DistributionApp.get_near_time_users()
+            if users:
+                delay_seconds = int((min_time - datetime.now()).total_seconds())
+                weekday = min_time.weekday()
+                for i, user in enumerate(users):
+                    print(user.get_group().get_schedule(day=days_of_week[weekday], week=current_week(),
+                                                        semester=1)) #now_semester()
+                    print(user)
+                    user.refresh_nearest_remind()
+                break
+
+
+if __name__ == '__main__':
+    DistributionApp.run()
