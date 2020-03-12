c00c0bc3b6ed66ba36e712dd2f2416368dfa19bf
Alexandr <ieaiaio@bk.ru>
2020-03-12 22:13:05 +0800

    Worked notify

--- notify_app/notify.py
+++ notify_app/notify.py
@@ -1,3 +1,4 @@
+import random
 from time import sleep
 
 from vk_api import vk_api
@@ -10,9 +11,9 @@ class NotifyApp:
     __timeout = 60
 
     def __init__(self):
-        vk = vk_api.VkApi(token=Config.VK_TOKEN)
+        self.vk = vk_api.VkApi(token=Config.VK_TOKEN)
         print('[*] Авторизация с ВКонтакте... ', end='')
-        vk._auth_token()
+        self.vk._auth_token()
         print('Успешно')
 
     # Возвращает ближайших по времени пользователей для отправки им расписания
@@ -33,36 +34,33 @@ class NotifyApp:
     @staticmethod
     def __is_changed_db():
         param = Meta.get_first('change_db_notify')
-        return param and param.get_and_change_boolean(boolean=False)
+        return param and param.get_and_change_boolean(new_status=False)
 
-
-    @staticmethod
-    def __send_notify(users, time: datetime):
+    def __send_notify(self, users, time: datetime):
         for i, user in enumerate(users):
-            weekday = time.weekday()
-            # print(user.get_group().get_schedule(day=days_of_week[weekday], week=current_week(),
-            #                                     semester=1)) #now_semester()
-            print(user)
-            print(user.group.name)
-            print(user.remind_date)
-            print(days_of_week[weekday])
             user.refresh_nearest_remind()
+            schedule = user.get_group().get_schedule(day=days_of_week[time.weekday()-4], week=2, semester=1)
+            message = '\n'.join('-%i: %s (%s)' % (item.number, item.name, item.cabinet if not None else 'ауд. -') for item in schedule)
+            self.vk.method("messages.send",
+                           {"peer_id": user.vk_id,
+                            "message": message,
+                            "random_id": random.randint(1, 2147483647)})
 
     # Отправляет сообщения ближайшим по времени пользователям с их расписанием и обновляет их время
-    @staticmethod
-    def run():
+    def run(self):
         while True:
             time, users = NotifyApp.__get_near_time_users()
+            # time = datetime.now() + timedelta(seconds=1)
             delay_seconds = int((time - datetime.now()).total_seconds())
-            while delay_seconds > 2:
-                sleep(2)
+            while delay_seconds > 0:
+                sleep(1)
                 if NotifyApp.__is_changed_db():
                     time, users = NotifyApp.__get_near_time_users()
                 delay_seconds = int((time - datetime.now()).total_seconds())
-
             if users:
-                NotifyApp.__send_notify(users, time)
+                self.__send_notify(users, time)
 
 
 if __name__ == '__main__':
-    NotifyApp.run()
+   notify = NotifyApp()
+   notify.run()
--- test.py
+++ test.py
@@ -1,5 +1,30 @@
-from app.menus import MenuTree
+def foo(faculties):
+    row_len = 0
+    result = 0
+    count_items_in_row = 0
+    count_rows = 0
+    limit_row_len = 30
+    limit_items_in_row = 4
+    limit_rows = 9
+    for faculty in faculties:
+        row_len += len(faculty)
+        print(faculty, end=', ')
+        count_items_in_row += 1
+        result += 1
+        if not ((count_items_in_row == 0) or (
+                row_len + len(faculty) < limit_row_len and count_items_in_row < limit_items_in_row)):
+            print('(%s, %s)' % (count_items_in_row, row_len))
+            count_items_in_row = 0
+            row_len = 0
+            count_rows += 1
+            if count_rows == limit_rows:
+                break
+    else:
+        print('(%s, %s)' % (count_items_in_row, row_len))
 
-menu = MenuTree()
-print(menu.get_schedule_of_group(list_answers=['ВПР41'])[0])
+    return result
 
+
+print(
+    foo(['1234567', '123', '12345', '123456789', '12345678', '123456789', '123456789', '12345678', '12345', '123456789',
+         '12345678', '123456789', '12345678', '12345', '123456789']))
