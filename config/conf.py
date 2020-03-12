c77c6584bcf8588a3906e09e16edc0faf78ccbbe
Alexandr <ieaiaio@bk.ru>
2020-03-13 00:01:48 +0800

    delete dir migrations

--- config/conf.py
+++ config/conf.py
@@ -14,12 +14,12 @@ class DevelopmentConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgres://cmalhdadykrvvc:645ec287e2d2c1593dfcf429a4738e5a022db27999a9d3f02ca260bc714f11d6@ec2-54-217-219-235.eu-west-1.compute.amazonaws.com:5432/dfh21ke69pne2k'
-    SQLALCHEMY_DATABASE_URI1 = 'postgres://postgres:taketake@dstubot.c1r6usn8actc.us-east-2.rds.amazonaws.com:5432/dstubot'
+    # SQLALCHEMY_DATABASE_URI1 = 'postgres://postgres:taketake@dstubot.c1r6usn8actc.us-east-2.rds.amazonaws.com:5432/dstubot'
     ENV = 'ProductionConfig'
     DEBUG = False
     TESTING = False
 
 
-Config = DevelopmentConfig
+Config = ProductionConfig
 
 
