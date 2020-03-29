a8ccb34ed30e68a6453fd7d4740595298bc8d73d
Alexandr <ieaiaio@bk.ru>
2020-03-29 12:38:33 +0800

    delete flask_sqlalchemy injection

--- web_app/routes.py
+++ web_app/routes.py
@@ -1,36 +1,36 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
-from web_app.run import db, flask_app, models_DB
+from web_app.run import flask_app, models_DB
 
 admin = Admin(flask_app, name='DataBase', url='/')
 
-admin.add_view(ModelView(models_DB.User, db.session, name='Пользователи', category='Пользователи'))
-admin.add_view(ModelView(models_DB.UserCache, db.session, name='Кэш пользователей', category='Пользователи'))
-admin.add_view(ModelView(models_DB.UserAnswer, db.session, name='Ответы пользователей', category='Пользователи'))
-admin.add_view(ModelView(models_DB.DinamicItems, db.session, name='Динамические пункты', category='Пользователи'))
-admin.add_view(ModelView(models_DB.Review, db.session, name='Отзывы', category='Пользователи'))
+admin.add_view(ModelView(models_DB.User, models_DB.session, name='Пользователи', category='Пользователи'))
+admin.add_view(ModelView(models_DB.UserCache, models_DB.session, name='Кэш пользователей', category='Пользователи'))
+admin.add_view(ModelView(models_DB.UserAnswer, models_DB.session, name='Ответы пользователей', category='Пользователи'))
+admin.add_view(ModelView(models_DB.DinamicItems, models_DB.session, name='Динамические пункты', category='Пользователи'))
+admin.add_view(ModelView(models_DB.Review, models_DB.session, name='Отзывы', category='Пользователи'))
 
-admin.add_view(ModelView(models_DB.Place, db.session, name='Места', category='Места'))
-admin.add_view(ModelView(models_DB.TypePlace, db.session, name='Тип места', category='Места'))
-admin.add_view(ModelView(models_DB.SchedulePlace, db.session, name='Расписание', category='Места'))
-admin.add_view(ModelView(models_DB.ManagerPlace, db.session, name='Управляющий', category='Места'))
-admin.add_view(ModelView(models_DB.Post, db.session, name='Должность управляющего', category='Места'))
+admin.add_view(ModelView(models_DB.Place, models_DB.session, name='Места', category='Места'))
+admin.add_view(ModelView(models_DB.TypePlace, models_DB.session, name='Тип места', category='Места'))
+admin.add_view(ModelView(models_DB.SchedulePlace, models_DB.session, name='Расписание', category='Места'))
+admin.add_view(ModelView(models_DB.ManagerPlace, models_DB.session, name='Управляющий', category='Места'))
+admin.add_view(ModelView(models_DB.Post, models_DB.session, name='Должность управляющего', category='Места'))
 
-admin.add_view(ModelView(models_DB.Faculty, db.session, name='Факультеты', category='Структура университета'))
-admin.add_view(ModelView(models_DB.Dean, db.session, name='Деканы', category='Структура университета'))
+admin.add_view(ModelView(models_DB.Faculty, models_DB.session, name='Факультеты', category='Структура университета'))
+admin.add_view(ModelView(models_DB.Dean, models_DB.session, name='Деканы', category='Структура университета'))
 admin.add_view(
-    ModelView(models_DB.ScheduleDeanOffice, db.session, name='Расписание деканата', category='Структура университета'))
-admin.add_view(ModelView(models_DB.Department, db.session, name='Кафедра', category='Структура университета'))
+    ModelView(models_DB.ScheduleDeanOffice, models_DB.session, name='Расписание деканата', category='Структура университета'))
+admin.add_view(ModelView(models_DB.Department, models_DB.session, name='Кафедра', category='Структура университета'))
 admin.add_view(
-    ModelView(models_DB.ManagerDepartment, db.session, name='Управляющие кафедрами', category='Структура университета'))
-admin.add_view(ModelView(models_DB.Specialty, db.session, name='Специальности', category='Структура университета'))
-admin.add_view(ModelView(models_DB.TypeSpecialty, db.session, name='Типы специальностей', category='Структура университета'))
+    ModelView(models_DB.ManagerDepartment, models_DB.session, name='Управляющие кафедрами', category='Структура университета'))
+admin.add_view(ModelView(models_DB.Specialty, models_DB.session, name='Специальности', category='Структура университета'))
+admin.add_view(ModelView(models_DB.TypeSpecialty, models_DB.session, name='Типы специальностей', category='Структура университета'))
 
-admin.add_view(ModelView(models_DB.Teacher, db.session, name='Преподаватели', category='Расписание'))
-admin.add_view(ModelView(models_DB.Group, db.session, name='Группы', category='Расписание'))
-admin.add_view(ModelView(models_DB.Subject, db.session, name='Предметы', category='Расписание'))
+admin.add_view(ModelView(models_DB.Teacher, models_DB.session, name='Преподаватели', category='Расписание'))
+admin.add_view(ModelView(models_DB.Group, models_DB.session, name='Группы', category='Расписание'))
+admin.add_view(ModelView(models_DB.Subject, models_DB.session, name='Предметы', category='Расписание'))
 
-admin.add_view(ModelView(models_DB.Log, db.session, name='Ошибки'))
-admin.add_view(ModelView(models_DB.Meta, db.session, name='Meta'))
+admin.add_view(ModelView(models_DB.Log, models_DB.session, name='Ошибки'))
+admin.add_view(ModelView(models_DB.Meta, models_DB.session, name='Meta'))
 
--- web_app/run.py
+++ web_app/run.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from config.conf import Config
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
-db = SQLAlchemy(flask_app)
 
 from app.models import models_DB
 from web_app.routes import *
